295d6ed2ee84d6caada57fe078041375cd850806
Thomas Tanner <44611591+sunyamare@users.noreply.github.com>
2019-08-14 22:36:16 +0200

    removed set_time_limit

--- www/control-panel-access.php
+++ www/control-panel-access.php
@@ -4,7 +4,6 @@
  * Developed by webButler (https://web-butler.ch)
  */
 ignore_user_abort(false);
-set_time_limit(30);
 
 $pathToFlushControl = "../w3tc-remote-flush-multiple.php";
 $correctPassword = 'password';
