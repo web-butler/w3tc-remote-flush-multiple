732cce4626a9924d0fa2afb03cdf3b2ef755afed
sunyamare <sunyamare@t-air.local>
2019-08-14 21:42:37 +0200

    style improvements

--- w3tc-remote-flush-multiple.php
+++ w3tc-remote-flush-multiple.php
@@ -3,26 +3,17 @@
  * Control Panel for flushing multiple websites' W3TC Plugin's Cache entirely
  * Developed by webButler (https://web-butler.ch)
  */
-ignore_user_abort( false );
+ignore_user_abort(false);
+set_time_limit(30);
 
-// Generate an array with websites and IDs from the JSON file
-$json = file_get_contents('../w3tc-remote-flush-multiple/websites.json'); // (relative to flush-admin-access.php , cause that's where it's being executed)
-$websitesArray = json_decode( $json, true );
-// print_r($websitesArray);
-
-// print basic html file with bootstrap css and some custom styles
-echo "<html>
-    <head>
-    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
-    <link rel='stylesheet' type='text/css' href='./style.css'/>
-    </head>
-    <body>
-    <div class='container' id='main-content'>
-    <h1>Flush W3TC Cache remotely</h1>
-    <p>..powered by:  <a href='https://web-butler.ch/' target='_blank'>webButler</a></p>";
-
-// retrieve information about what needs to be flushed
-$toFlush = $_GET;
+// retrieve information about which websites need to be flushed
+if (isset($_POST)) {
+    $websitesToFlush = $_POST;
+}
+// Generate an object with websites and IDs from the JSON file
+// (relative to flush-admin-access.php , cause that's where it's being executed)
+$jsonFile = file_get_contents('../w3tc-remote-flush-multiple/websites.json'); 
+$websiteArray = json_decode( $jsonFile, true );
 
 // define startsWith to filter stage envs (will output true if string starts with startString)
 function startsWith ($string, $startString) 
@@ -31,12 +22,11 @@ function startsWith ($string, $startString)
     return (substr($string, 0, $len) === $startString); 
 } 
 
-// if nothing was chosen to be flushed, create form to choose what needs to be flushed
-if (!$toFlush) {
+function createFlushForm() {
     echo "<h2>Choose the website(s) to flush</h2>
-	<p>Caution: it's wiser to choose only one website at a time! (when do you really need to flush several websites at once?)</p>
+    <p>Caution: it's wiser to choose only one website at a time! (when do you really need to flush several websites at once?)</p>
     <div class='columns'>
-    <form action=''>";
+    <form action='' method='post'>";
     // in case the page is a stage and has page url, user, and pass as array as value, choose the page
     function createCheckboxes($pageOrArray,$key) {
         if (is_array($pageOrArray)) {
@@ -52,31 +42,32 @@ if (!$toFlush) {
         echo $page;
         echo "</label><br />";
     }
-    array_walk($websitesArray,"createCheckboxes"); // throws the array into the function for each element
+    global $websiteArray;
+    array_walk($websiteArray,"createCheckboxes"); // throws the array into the function for each element
     echo "</div>
     <input type='submit' value='Flush All W3TC Cache'>
     </form>";
-// if something needs to be flushed, do that using curl
-} else {
+}
+
+function flushWebsites() {
     echo "<h2>Results are here:</h2>";
-    function flushWebsites($page,$key) {
+    function doCurl($page,$key) {
+        $isStaging = startsWith($page,'stage.');
         // in case the page is a stage and has page url, user, and pass as array as value, assign variables accordingly
-        if (startsWith($page,'stage')) {
-            global $websitesArray;
-            $array = $websitesArray[$key];
-            $username = $array[1];
-            $password = $array[2];
+        if ($isStaging) {
+            global $websiteArray;
+            $stagingArray = $websiteArray[$key];
+            $username = $stagingArray[1];
+            $password = $stagingArray[2];
         }
         $url = 'https://' . $page . '/flush-script.php?pass=password';
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_TIMEOUT, 30); // set max. execution time for the curl
         curl_setopt($ch, CURLOPT_POST, 1); // require post
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // make curl_exec return instead of output data
-        // print_r($username);
-        // print_r($password);
         echo '<p>';
-        if (startsWith($page,'stage')) {
-            // check if the site is a stage, and if so, add user and password
+        // check if the site is a stage, and if so, add user and password
+        if ($isStaging) {
             curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
             curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
             echo "[http digest auth] ";
@@ -85,7 +76,7 @@ if (!$toFlush) {
         $curl_result = curl_exec($ch);
         $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE); // store http status
         if ($http_status != 200) {
-            echo 'Error'; // if curl doesnt' return http status 200, print 'Error'
+            echo 'Error'; // if curl doesn't return http status 200, print 'Error'
         } else {
             echo $curl_result; // otherwise print text in remote script (here: 'Script executed')
         };
@@ -97,11 +88,17 @@ if (!$toFlush) {
         echo '</p>';
         curl_close($ch);
     }
-    array_walk($toFlush,"flushWebsites"); // throws the array into the function for each element
-    echo "<form action=''>
-    <input type='submit' value='Reset and flush again/more'>
-    </form> 
-    </div>
-    </body>
-    </html>";
+    global $websitesToFlush;
+    array_walk($websitesToFlush,"doCurl"); // throws the array into the function for each element
+echo "<form action=''>
+<input type='submit' value='Reset and flush again/more'>
+</form>";
+}
+
+// if nothing was chosen to be flushed, create form to choose what needs to be flushed
+if (!$websitesToFlush) {
+    createFlushForm();
+// if something needs to be flushed, do that using curl
+} else {
+    flushWebsites();
 }
---
+++ www/control-panel-access.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * Basic security to access the flashing functionality using a browser cookie
+ * Developed by webButler (https://web-butler.ch)
+ */
+ignore_user_abort(false);
+set_time_limit(30);
+
+$pathToFlushControl = "../w3tc-remote-flush-multiple/w3tc-remote-flush-multiple.php";
+$correctPassword = 'password';
+$cookieName = 'WEB_BUTLER_W3TC_FLUSH';
+$browserCookie = '';
+$passwordFromGet = '';
+$authenticated = false;
+
+if (isset($_COOKIE[$cookieName])) {
+    $browserCookie = $_COOKIE[$cookieName];
+}
+if (isset($_POST['password'])) {
+    $passwordFromGet = $_POST['password'];
+}
+
+// 1: if the cookie is correct, authenticate
+if ($browserCookie === $correctPassword) {
+    $authenticated = true;
+// 2: if the password is correct but there is no cookie, reset the cookie correctly and reload the page
+} elseif ($passwordFromGet === $correctPassword) {
+    // "unset" cookie by assigning no value and putting the expiration date in the past
+    setcookie($cookieName, '', 1);
+    setcookie($cookieName, $correctPassword, time()+3600); // 3600s = 1 hour
+    header("Location:".basename(__FILE__));
+// if either, the cookie or pw is wrong, show the login form again
+} elseif ($browserCookie !== $correctPassword || $passwordFromGet !== $correctPassword) {
+    $authenticated = false;
+} else { // otherwise, print the browser cookie and password from GET
+    echo "Smth's odd here ...";
+    echo "The Browser's Cookie Value: ".$browserCookie;
+    echo "The password received is: ".$passwordFromGet;
+    exit;
+};
+
+function showLoginForm() {
+    echo "<h2>You have to enter the password to view this content!</h2>
+    <form action='' method='post'>
+        <input type='password' name='password'>
+        <br />
+        <input type='submit' value='Login'>
+    </form>
+    ";
+}
+?>
+
+<html>
+<head>
+<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+<link rel='stylesheet' type='text/css' href='./style.css'/>
+</head>
+<body>
+<div class='container' id='main-content'>
+    <h1>Flush W3TC Cache remotely</h1>
+    <p>..powered by:  <a href='https://web-butler.ch/' target='_blank'>webButler</a></p>
+    <?php
+    if ($authenticated) {
+        include($pathToFlushControl);
+    } else {
+        showLoginForm();
+    }
+    ?>
+</div>
+</body>
+</html>
